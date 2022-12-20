3bda7d153ebd46ad62c8cc471e578093fa34445d
Iuliia <itushkova@gmail.com>
2022-12-20 17:24:33 +0100

    bonhomme

--- src/components/BonHomme.vue
+++ src/components/BonHomme.vue
@@ -1,148 +1,221 @@
+<template>
+    <div>
+      <div id="container"></div>
+      <div id="info">
+          <a href="https://threejs.org" target="_blank" rel="noopener">three.js</a> - Skeletal Additive Animation Blending
+          (model from <a href="https://www.mixamo.com/" target="_blank" rel="noopener">mixamo.com</a>)<br/>
+      </div>
+    </div>
+</template>
+
 <script lang="ts" setup>
 /* eslint-disable */
-import * as THREE from 'three';
+    import { onMounted } from 'vue';
+    import * as THREE from 'three';
+    import Stats from 'three/examples/jsm/libs/stats.module.js';
+    import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-            import Stats from 'three/examples/jsm/libs/stats.module.js';
-			import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-			import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-			import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+    let scene, renderer, camera, stats;
+    let model, skeleton, mixer, clock;
 
-			let scene, renderer, camera, stats;
-			let model, skeleton, mixer, clock;
+    const crossFadeControls = [];
 
-			const crossFadeControls = [];
+    let currentBaseAction = 'idle';
+    const allActions = [];
+    const baseActions = {
+        idle: { weight: 1 },
+        walk: { weight: 0 },
+        run: { weight: 0 }
+    };
+    const additiveActions = {
+        sneak_pose: { weight: 0 },
+        sad_pose: { weight: 0 },
+        agree: { weight: 0 },
+        headShake: { weight: 0 }
+    };
+    let panelSettings, numAnimations;
 
-			let currentBaseAction = 'idle';
-			const allActions = [];
-			const baseActions = {
-				idle: { weight: 1 },
-				walk: { weight: 0 },
-				run: { weight: 0 }
-			};
-			const additiveActions = {
-				sneak_pose: { weight: 0 },
-				sad_pose: { weight: 0 },
-				agree: { weight: 0 },
-				headShake: { weight: 0 }
-			};
-			let panelSettings, numAnimations;
+    onMounted(() => {
+        init();
+    });
 
-			init();
+	
 
-			function init() {
+    function init() {
 
-				const container = document.getElementById( 'container' );
-				clock = new THREE.Clock();
+        const container = document.getElementById('container');
 
-				scene = new THREE.Scene();
-				scene.background = new THREE.Color( 0xa0a0a0 );
-				scene.fog = new THREE.Fog( 0xa0a0a0, 10, 50 );
+        clock = new THREE.Clock();
+        scene = new THREE.Scene();
 
-				const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-				hemiLight.position.set( 0, 20, 0 );
-				scene.add( hemiLight );
+        
+        scene.background = new THREE.Color(	'#9ADFF8' );
+        scene.fog = new THREE.Fog( '#F0FFFF', 20, 100);
 
-				const dirLight = new THREE.DirectionalLight( 0xffffff );
-				dirLight.position.set( 3, 10, 10 );
-				dirLight.castShadow = true;
-				dirLight.shadow.camera.top = 2;
-				dirLight.shadow.camera.bottom = - 2;
-				dirLight.shadow.camera.left = - 2;
-				dirLight.shadow.camera.right = 2;
-				dirLight.shadow.camera.near = 0.1;
-				dirLight.shadow.camera.far = 40;
-				scene.add( dirLight );
 
-				// ground
+    //GROUND texture 
+    //     const gt = new THREE.TextureLoader().load( 'textures/grass-1.jpg' );
+    //     const gg = new THREE.PlaneGeometry( 16000, 16000 );
+    //     const gm = new THREE.MeshPhongMaterial( { color: 0xffffff, map: gt } );
 
-				const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-				mesh.rotation.x = - Math.PI / 2;
-				mesh.receiveShadow = true;
-				scene.add( mesh );
+    //     const ground = new THREE.Mesh( gg, gm ); 
+    //     ground.rotation.x = - Math.PI / 2;
+    //     ground.material.map.repeat.set( 64, 64 );
+    //     ground.material.map.wrapS = THREE.RepeatWrapping;
+    //     ground.material.map.wrapT = THREE.RepeatWrapping;
+    //     ground.material.map.encoding = THREE.sRGBEncoding;
+    //    // note that because the ground does not cast a shadow, .castShadow is left false
+    //     ground.receiveShadow = true;
 
-				const loader = new GLTFLoader();
-				loader.load( 'models/gltf/Xbot.glb', function ( gltf ) {
+    //     scene.add( ground );
 
-					model = gltf.scene;
-					scene.add( model );
 
-					model.traverse( function ( object ) {
+        //GROUND unicolor
+        // let groundMesh;
+        // const groundGeometry = new THREE.BoxGeometry( 30, 0.01, 40 );
+        // const groundMaterial = new THREE.MeshLambertMaterial( { color: 'red' } );
+        // groundMesh = new THREE.Mesh( groundGeometry, groundMaterial );
+        // groundMesh.position.y = 0.0; //this value must be slightly lower than the planeConstant (0.01) parameter above
+        // scene.add( groundMesh );
 
-						if ( object.isMesh ) object.castShadow = true;
+        // ground
+        const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 5, 100 ), new THREE.MeshPhongMaterial( { color: '#1A1A1A', depthWrite: false } ) );
+        mesh.rotation.x = - Math.PI / 2;
+        mesh.receiveShadow = true;
+        scene.add( mesh );
 
-					} );
 
-					skeleton = new THREE.SkeletonHelper( model );
-					skeleton.visible = false;
-					scene.add( skeleton );
+        //middle line
 
-					const animations = gltf.animations;
-					mixer = new THREE.AnimationMixer( model );
+        const meshMiddleLine = new THREE.Mesh( new THREE.PlaneGeometry( 0.1, 100 ), new THREE.MeshPhongMaterial( { color: '#FFFFFF', depthWrite: false } ) );
+        meshMiddleLine.rotation.x = - Math.PI / 2;
+        meshMiddleLine.receiveShadow = true;
+        scene.add( meshMiddleLine );
 
-					numAnimations = animations.length;
+        //left line
 
-					for ( let i = 0; i !== numAnimations; ++ i ) {
+        const meshLeftLine = new THREE.Mesh( new THREE.PlaneGeometry( 0.1, 100 ), new THREE.MeshPhongMaterial( { color: '#FFFFFF', depthWrite: false } ) );
+        meshLeftLine.rotation.x = - Math.PI / 2;
+        meshLeftLine.receiveShadow = true;
+        meshLeftLine.position.x = -2.5;
+        scene.add( meshLeftLine );
 
-						let clip = animations[ i ];
-						const name = clip.name;
+        //right line
 
-						if ( baseActions[ name ] ) {
+        const meshRightLine = new THREE.Mesh( new THREE.PlaneGeometry( 0.1, 100 ), new THREE.MeshPhongMaterial( { color: '#FFFFFF', depthWrite: false } ) );
+        meshRightLine.rotation.x = - Math.PI / 2;
+        meshRightLine.receiveShadow = true;
+        meshRightLine.position.x = 2.5;
+        scene.add( meshRightLine );
 
-							const action = mixer.clipAction( clip );
-							activateAction( action );
-							baseActions[ name ].action = action;
-							allActions.push( action );
 
-						} else if ( additiveActions[ name ] ) {
 
-							// Make the clip additive and remove the reference frame
+        //LUMIERE
 
-							THREE.AnimationUtils.makeClipAdditive( clip );
+        const hemiLight = new THREE.HemisphereLight( 0xffffff, "#f2b705");
+        hemiLight.position.set( 0, 20, 0 );
+        scene.add( hemiLight );
 
-							if ( clip.name.endsWith( '_pose' ) ) {
+        const dirLight = new THREE.DirectionalLight( 0xffffff );
+        dirLight.position.set( 20, 10, 10 );
+        dirLight.castShadow = true;
+        dirLight.shadow.camera.top = 7;
+        dirLight.shadow.camera.bottom = - 2;
+        dirLight.shadow.camera.left = - 4;
+        dirLight.shadow.camera.right = 2;
+        dirLight.shadow.camera.near = 0.1;
+        dirLight.shadow.camera.far = 40;
+        scene.add( dirLight );
 
-								clip = THREE.AnimationUtils.subclip( clip, clip.name, 2, 3, 30 );
+        
+        //----------- MODEL ------------------------------------
+        const loader = new GLTFLoader();
 
-							}
+        const stepPerson = 'models/gltf/Xbot.glb';
+        // const rumba = 'models/gltf/test/scene.gltf';
+        // const salsa = 'models/gltf/salsaDancing.fbx';
 
-							const action = mixer.clipAction( clip );
-							activateAction( action );
-							additiveActions[ name ].action = action;
-							allActions.push( action );
+        loader.load( stepPerson, function ( gltf ) {
 
-						}
+            model = gltf.scene;
+            scene.add( model );
 
-					}
+            model.traverse( function ( object ) {
 
-					createPanel();
+                if ( object.isMesh ) object.castShadow = true;
 
-					animate();
+            } );
 
-				} );
+            skeleton = new THREE.SkeletonHelper( model );
+            skeleton.visible = false;
+            scene.add( skeleton );
 
-				renderer = new THREE.WebGLRenderer( { antialias: true } );
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				renderer.outputEncoding = THREE.sRGBEncoding;
-				renderer.shadowMap.enabled = true;
-				container.appendChild( renderer.domElement );
+            const animations = gltf.animations;
+            mixer = new THREE.AnimationMixer( model );
 
-				// camera
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 100 );
-				camera.position.set( - 1, 2, 3 );
+            numAnimations = animations.length;
 
-				const controls = new OrbitControls( camera, renderer.domElement );
-				controls.enablePan = false;
-				controls.enableZoom = false;
-				controls.target.set( 0, 1, 0 );
-				controls.update();
+            for ( let i = 0; i !== numAnimations; ++ i ) {
 
-				stats = new Stats();
-				container.appendChild( stats.dom );
+                let clip = animations[ i ];
+                const name = clip.name;
 
-				window.addEventListener( 'resize', onWindowResize );
+                if ( baseActions[ name ] ) {
 
-			}
+                    const action = mixer.clipAction( clip );
+                    activateAction( action );
+                    baseActions[ name ].action = action;
+                    allActions.push( action );
+
+                } else if ( additiveActions[ name ] ) {
+
+                    // Make the clip additive and remove the reference frame
+
+                    THREE.AnimationUtils.makeClipAdditive( clip );
+
+                    if ( clip.name.endsWith( '_pose' ) ) {
+
+                        clip = THREE.AnimationUtils.subclip( clip, clip.name, 2, 3, 30 );
+
+                    }
+
+                    const action = mixer.clipAction( clip );
+                    activateAction( action );
+                    additiveActions[ name ].action = action;
+                    allActions.push( action );
+                }
+            }
+
+           createPanel();
+
+           animate();
+
+        } );
+
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.shadowMap.enabled = true;
+        container.appendChild( renderer.domElement );
+
+        // camera
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 100 );
+        camera.position.set( - 1, 2, 3 );
+
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.enablePan = false;
+        controls.enableZoom = false;
+        controls.target.set( 0, 1, 0 );
+        controls.update();
+
+        stats = Stats();
+        container.appendChild( stats.dom );
+
+        window.addEventListener( 'resize', onWindowResize );
+    }
 
 			function createPanel() {
 
@@ -159,71 +232,44 @@ import * as THREE from 'three';
 				const baseNames = [ 'None', ...Object.keys( baseActions ) ];
 
 				for ( let i = 0, l = baseNames.length; i !== l; ++ i ) {
-
 					const name = baseNames[ i ];
 					const settings = baseActions[ name ];
 					panelSettings[ name ] = function () {
-
 						const currentSettings = baseActions[ currentBaseAction ];
 						const currentAction = currentSettings ? currentSettings.action : null;
 						const action = settings ? settings.action : null;
 
-						if ( currentAction !== action ) {
-			
-							prepareCrossFade( currentAction, action, 0.35 );
-			
-						}
-
+						if ( currentAction !== action ) prepareCrossFade( currentAction, action, 0.35 );
 					};
-
 					crossFadeControls.push( folder1.add( panelSettings, name ) );
-
 				}
 
 				for ( const name of Object.keys( additiveActions ) ) {
-
 					const settings = additiveActions[ name ];
-
 					panelSettings[ name ] = settings.weight;
 					folder2.add( panelSettings, name, 0.0, 1.0, 0.01 ).listen().onChange( function ( weight ) {
-
 						setWeight( settings.action, weight );
 						settings.weight = weight;
-
 					} );
-
 				}
 
 				folder3.add( panelSettings, 'modify time scale', 0.0, 1.5, 0.01 ).onChange( modifyTimeScale );
-
 				folder1.open();
 				folder2.open();
 				folder3.open();
 
 				crossFadeControls.forEach( function ( control ) {
-
 					control.setInactive = function () {
-
 						control.domElement.classList.add( 'control-inactive' );
-
 					};
-
 					control.setActive = function () {
-
-						control.domElement.classList.remove( 'control-inactive' );
-
+					    control.domElement.classList.remove( 'control-inactive' );
 					};
-
 					const settings = baseActions[ control.property ];
-
 					if ( ! settings || ! settings.weight ) {
-
 						control.setInactive();
-
 					}
-
 				} );
-
 			}
 
 			function activateAction( action ) {
@@ -232,111 +278,65 @@ import * as THREE from 'three';
 				const settings = baseActions[ clip.name ] || additiveActions[ clip.name ];
 				setWeight( action, settings.weight );
 				action.play();
-
 			}
 
 			function modifyTimeScale( speed ) {
-
-				mixer.timeScale = speed;
-
+			    mixer.timeScale = speed;
 			}
 
 			function prepareCrossFade( startAction, endAction, duration ) {
-
 				// If the current action is 'idle', execute the crossfade immediately;
 				// else wait until the current action has finished its current loop
 
 				if ( currentBaseAction === 'idle' || ! startAction || ! endAction ) {
-
 					executeCrossFade( startAction, endAction, duration );
-
 				} else {
-
 					synchronizeCrossFade( startAction, endAction, duration );
-
 				}
-
 				// Update control colors
-
 				if ( endAction ) {
-
 					const clip = endAction.getClip();
 					currentBaseAction = clip.name;
-
 				} else {
-
 					currentBaseAction = 'None';
-
 				}
-
 				crossFadeControls.forEach( function ( control ) {
-
 					const name = control.property;
-
 					if ( name === currentBaseAction ) {
-
 						control.setActive();
-
 					} else {
-
 						control.setInactive();
-
 					}
-
 				} );
-
 			}
 
 			function synchronizeCrossFade( startAction, endAction, duration ) {
-
 				mixer.addEventListener( 'loop', onLoopFinished );
-
 				function onLoopFinished( event ) {
-
 					if ( event.action === startAction ) {
-
 						mixer.removeEventListener( 'loop', onLoopFinished );
-
 						executeCrossFade( startAction, endAction, duration );
-
 					}
-
 				}
-
 			}
 
 			function executeCrossFade( startAction, endAction, duration ) {
-
 				// Not only the start action, but also the end action must get a weight of 1 before fading
 				// (concerning the start action this is already guaranteed in this place)
-
 				if ( endAction ) {
-
 					setWeight( endAction, 1 );
 					endAction.time = 0;
-
 					if ( startAction ) {
-
 						// Crossfade with warping
-
 						startAction.crossFadeTo( endAction, duration, true );
-
 					} else {
-
 						// Fade in
-
 						endAction.fadeIn( duration );
-
 					}
-
 				} else {
-
 					// Fade out
-
 					startAction.fadeOut( duration );
-
 				}
-
 			}
 
 			// This function is needed, since animationAction.crossFadeTo() disables its start action and sets
@@ -390,15 +390,7 @@ import * as THREE from 'three';
 
 </script>
 
-<template>
-      <div>
-        <div id="container"></div>
-		<div id="info">
-			<a href="https://threejs.org" target="_blank" rel="noopener">three.js</a> - Skeletal Additive Animation Blending
-			(model from <a href="https://www.mixamo.com/" target="_blank" rel="noopener">mixamo.com</a>)<br/>
-		</div>
-      </div>
-</template>
+
 
 
 <style>
